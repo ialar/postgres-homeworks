53a134dd2f647553bceba199673b05868782f09d
Igor Larionov <jedialar@gmail.com>
2023-08-06 22:36:09 +0300

    added code to create_tables.sql and main.py

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id	serial PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(5) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id serial PRIMARY KEY,
+	customer_id varchar(5),
+	employee_id int,
+	order_date date,
+	ship_city varchar(100),
+	FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+	FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,54 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import os
+from pathlib import Path
+
+import psycopg2
+
+employees_data_path = str(Path(__file__).resolve().parent / 'north_data' / 'employees_data.csv')
+customers_data_path = str(Path(__file__).resolve().parent / 'north_data' / 'customers_data.csv')
+orders_data_path = str(Path(__file__).resolve().parent / 'north_data' / 'orders_data.csv')
+
+conn = psycopg2.connect(database='north', user='postgres', password=os.getenv("PSQL_PASS"))
+
+
+def fill_employees_data(path):
+    with open(path) as file:
+        reader = csv.reader(file)
+        next(reader)
+        with conn.cursor() as cur:
+            cur.executemany('INSERT INTO employees '
+                            '(employee_id, first_name, last_name, title, birth_date, notes) '
+                            'VALUES (%s, %s, %s, %s, %s, %s)', reader)
+        conn.commit()
+
+
+def fill_customers_data(path):
+    with open(path) as file:
+        reader = csv.reader(file)
+        next(reader)
+        with conn.cursor() as cur:
+            cur.executemany('INSERT INTO customers '
+                            '(customer_id, company_name, contact_name) '
+                            'VALUES (%s, %s, %s)', reader)
+        conn.commit()
+
+
+def fill_orders_data(path):
+    with open(path) as file:
+        reader = csv.reader(file)
+        next(reader)
+        with conn.cursor() as cur:
+            cur.executemany('INSERT INTO orders '
+                            '(order_id, customer_id, employee_id, order_date, ship_city) '
+                            'VALUES (%s, %s, %s, %s, %s)', reader)
+        conn.commit()
+
+
+if __name__ == "__main__":
+    try:
+        fill_employees_data(path=employees_data_path)
+        fill_customers_data(path=customers_data_path)
+        fill_orders_data(path=orders_data_path)
+    finally:
+        conn.close()
